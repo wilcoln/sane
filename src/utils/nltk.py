0a88901f5f81da0a3c1ec16cecd4c730ed7433aa
Wilfried Bounsi <wilcoln99@gmail.com>
2022-08-04 22:24:40 +0100

    fix str cast

--- src/utils/nltk.py
+++ src/utils/nltk.py
@@ -4,4 +4,5 @@ from nltk.util import everygrams
 
 
 def all_grams(sentence) -> List[str]:
+    sentence = str(sentence)
     return set('_'.join(ngram) for ngram in everygrams(sentence.split(), min_len=1, max_len=len(sentence.split(' '))))
