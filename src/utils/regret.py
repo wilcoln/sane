3ff58978facdcf658ed4aa2e5f24416b079a64b1
Wilfried Bounsi <wilcoln99@gmail.com>
2022-08-11 09:57:07 +0100

    update regret loss

--- src/utils/regret.py
+++ src/utils/regret.py
@@ -1,8 +1,8 @@
 import torch
 
 
-def regret(loss, expert_loss, reduce=True):
-    result = torch.exp(-expert_loss) * loss
+def regret(loss, expert_loss, reduce=True, eps=1e-8):
+    result = (torch.exp(-expert_loss) / (eps + expert_loss)) * loss
     if reduce:
         return result.mean()
     return result
@@ -10,7 +10,7 @@ def regret(loss, expert_loss, reduce=True):
 
 # Test regret
 if __name__ == '__main__':
-    loss = torch.FloatTensor([1, 1, 0, 0, 1])
-    expert_loss = torch.FloatTensor([0, 0, 0, .1, .1])
+    loss = torch.FloatTensor([1, 1, 0, 0, 1, 1])
+    expert_loss = torch.FloatTensor([0, 0, 0, 1, 1, 100])
     print(regret(loss, expert_loss, reduce=False))
     print(regret(loss, expert_loss))
