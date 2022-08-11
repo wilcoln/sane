9f1d6be3b177fe34161fa3517d9385b5a77ac7c5
Wilfried Bounsi <wilcoln99@gmail.com>
2022-08-11 17:20:58 +0100

    try out several regret functions

--- src/utils/regret.py
+++ src/utils/regret.py
@@ -1,8 +1,13 @@
 import torch
-
+import torch.nn.functional as F
 
 def regret(loss, expert_loss, reduce=True, eps=1e-8):
-    result = (torch.exp(-expert_loss) / (eps + expert_loss)) * loss
+    # result = torch.exp(-expert_loss) * loss
+    # result = (torch.exp(-expert_loss) / (eps + expert_loss)) * loss
+    # result = (torch.exp(-expert_loss) * loss / (eps + expert_loss)) * loss
+    # result = torch.exp(-expert_loss) * torch.norm(loss - expert_loss)
+    result = F.relu(loss - expert_loss)
+    # result = (F.relu(loss - expert_loss) / torch.norm(loss - expert_loss)) * loss
     if reduce:
         return result.mean()
     return result
