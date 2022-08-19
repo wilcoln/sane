4ea2dd009588cbcce2fac314782603640d900867
Wilfried Bounsi <wilcoln99@gmail.com>
2022-08-19 12:57:16 +0100

    progress on adding comve

---
+++ src/preprocessing/benchmark.py
@@ -0,0 +1,93 @@
+import math
+import os
+import os.path as osp
+from icecream import ic
+from tqdm import tqdm
+
+from src.settings import settings
+from src.conceptnet import conceptnet as cn
+from src.utils.embeddings import bart
+from src.utils.types import ChunkedList
+
+
+def encode(splits, output_dir):
+    for split, split_set in splits.items():
+        sentences_embedding_path = osp.join(output_dir, f'{split}_Sentences_embedding')
+        try:
+            # Load Sentences_embeddings
+            ic('Loading Sentences_embeddings')
+            split_set['Sentences_embedding'] = ChunkedList(n=len(split_set['Sentences']),
+                                                           dirpath=sentences_embedding_path)
+        except FileNotFoundError:
+            ic('Computing Sentences_embeddings')
+            # Create sentence embeddings
+            split_set['Sentences_embedding'] = ChunkedList(lst=split_set['Sentences'], num_chunks=math.ceil(
+                len(split_set['Sentences']) / settings.chunk_size)).apply(lambda l: bart(l, verbose=True),
+                                                                          dirpath=sentences_embedding_path)
+        # Update split_set
+        splits[split] = split_set
+    return splits
+
+
+def df_to_dict(splits):
+    return {split: split_set.to_dict(orient='list') for split, split_set in splits.items()}
+
+
+def save_splits(splits, output_dir):
+    for split, split_set in splits.items():
+        for k, v in split_set.items():
+            if not isinstance(v, ChunkedList):
+                k_path = osp.join(output_dir, f'{split}_{k}')
+                ChunkedList(lst=v, num_chunks=math.ceil(len(split_set['Sentences']) / settings.chunk_size)).to_big(
+                    k_path)
+
+
+def compute_concept_ids(sentence_list):
+    return [
+        cn.nodes2ids(cn.subgraph(cn.search(sentence), radius=1).nodes)
+        for sentence in tqdm(sentence_list)
+    ]
+
+
+def add_concepts(splits, esnli_output_dir):
+    ic('Add Knowledge to Data Points')
+    for split, split_set in splits.items():
+        concepts_path = os.path.join(esnli_output_dir, f'{split}_concept_ids')
+        try:
+            ic(f'Loading Concept ids for {split} split')
+            split_set['concept_ids'] = ChunkedList(n=len(split_set['Sentences']), dirpath=concepts_path)
+        except FileNotFoundError:
+            ic(f'Computing concept ids for {split} split')
+            split_set['concept_ids'] = ChunkedList(lst=split_set['Sentences'], num_chunks=math.ceil(
+                len(split_set['Sentences']) / settings.chunk_size)).apply(
+                lambda l: compute_concept_ids(l), concepts_path)
+        # Update split_set
+        splits[split] = split_set
+
+    return splits
+
+
+def preprocess(read_dataset, reduce_dataset, data_frac: float = .01):
+    esnli_output_dir = osp.join(settings.data_dir, f'esnli_{settings.data_frac}')
+    if not os.path.exists(esnli_output_dir):
+        os.mkdir(esnli_output_dir)
+
+    # Read dataset
+    ic('Reading dataset')
+    splits = read_dataset()
+    # Reduce dataset
+    ic('Reducing dataset')
+    splits = reduce_dataset(splits, data_frac)
+    # Convert to dict
+    ic('Converting to dict')
+    splits = df_to_dict(splits)
+    # Add concepts
+    ic('Adding concepts')
+    splits = add_concepts(splits, esnli_output_dir)
+    # Encode sentences
+    ic('Encoding sentences')
+    splits = encode(splits, esnli_output_dir)
+    # Save splits
+    ic('Saving splits')
+    save_splits(splits, esnli_output_dir)
+    ic('Done')
--- src/preprocessing/comve.py
+++ src/preprocessing/comve.py
@@ -1,16 +1,10 @@
-import math
-import os
 import os.path as osp
 import random
 
 import pandas as pd
-from icecream import ic
-from tqdm import tqdm
 
-from src.conceptnet import conceptnet as cn
+from src.preprocessing.benchmark import preprocess
 from src.settings import settings
-from src.utils.embeddings import bart
-from src.utils.types import ChunkedList
 
 
 def read_dataset():
@@ -40,99 +34,20 @@ def reduce_dataset(splits, frac):
             sentences.append(sentence)
             gold_labels.append(label)
 
-        split_set['Sentences'] = sentences
-        split_set['gold_label'] = gold_labels
+        split_set['Sentences'] = pd.Series(sentences)
+        split_set['gold_label'] = pd.Series(gold_labels)
         # Drop useless columns
         useless_columns = ['Confusing Reason1', 'Confusing Reason2']
         split_set.drop(columns=useless_columns, inplace=True, errors='ignore')
+
+        # Rename explanation columns
+        split_set = split_set.rename(columns={f'Right Reason{i}': f'Explanation_{i}' for i in range(1, 4)})
+
         # Update split_set
         splits[split] = split_set
 
     return splits
-
-
-def encode(splits, output_dir):
-    for split, split_set in splits.items():
-        sentences_embedding_path = osp.join(output_dir, f'{split}_Sentences_embedding')
-        try:
-            # Load Sentences_embeddings
-            ic('Loading Sentences_embeddings')
-            split_set['Sentences_embedding'] = ChunkedList(n=len(split_set['Sentences']),
-                                                           dirpath=sentences_embedding_path)
-        except FileNotFoundError:
-            ic('Computing Sentences_embeddings')
-            # Create sentence embeddings
-            split_set['Sentences_embedding'] = ChunkedList(lst=split_set['Sentences'], num_chunks=math.ceil(
-                len(split_set['Sentences']) / settings.chunk_size)).apply(lambda l: bart(l, verbose=True),
-                                                                          dirpath=sentences_embedding_path)
-        # Update split_set
-        splits[split] = split_set
-    return splits
-
-
-def df_to_dict(splits):
-    return {split: split_set.to_dict(orient='list') for split, split_set in splits.items()}
-
-
-def save_splits(splits, output_dir):
-    for split, split_set in splits.items():
-        for k, v in split_set.items():
-            if not isinstance(v, ChunkedList):
-                k_path = osp.join(output_dir, f'{split}_{k}')
-                ChunkedList(lst=v, num_chunks=math.ceil(len(split_set['Sentences']) / settings.chunk_size)).to_big(
-                    k_path)
-
-
-def compute_concept_ids(sentence_list):
-    return [
-        cn.nodes2ids(cn.subgraph(cn.search(sentence), radius=1).nodes)
-        for sentence in tqdm(sentence_list)
-    ]
-
-
-def add_concepts(splits, comve_output_dir):
-    ic('Add Knowledge to Data Points')
-    for split, split_set in splits.items():
-        concepts_path = os.path.join(comve_output_dir, f'{split}_concept_ids')
-        try:
-            ic(f'Loading Concept ids for {split} split')
-            split_set['concept_ids'] = ChunkedList(n=len(split_set['Sentences']), dirpath=concepts_path)
-        except FileNotFoundError:
-            ic(f'Computing concept ids for {split} split')
-            split_set['concept_ids'] = ChunkedList(lst=split_set['Sentences'], num_chunks=math.ceil(
-                len(split_set['Sentences']) / settings.chunk_size)).apply(
-                lambda l: compute_concept_ids(l), concepts_path)
-        # Update split_set
-        splits[split] = split_set
-
-    return splits
-
-
-def preprocess(comve_frac: float = .01):
-    comve_output_dir = osp.join(settings.data_dir, f'comve_{settings.data_frac}')
-    if not os.path.exists(comve_output_dir):
-        os.mkdir(comve_output_dir)
-
-    # Read dataset
-    ic('Reading dataset')
-    splits = read_dataset()
-    # Reduce dataset
-    ic('Reducing dataset')
-    splits = reduce_dataset(splits, comve_frac)
-    # Convert to dict
-    ic('Converting to dict')
-    splits = df_to_dict(splits)
-    # Add concepts
-    ic('Adding concepts')
-    splits = add_concepts(splits, comve_output_dir)
-    # Encode sentences
-    ic('Encoding sentences')
-    splits = encode(splits, comve_output_dir)
-    # Save splits
-    ic('Saving splits')
-    save_splits(splits, comve_output_dir)
-    ic('Done')
 
 
 if __name__ == 'main':
-    preprocess(settings.data_frac)
+    preprocess(read_dataset, reduce_dataset, settings.data_frac)
