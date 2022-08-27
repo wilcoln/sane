89a5df4898bb0d835ea04586b0308b99aa7d46c8
Wilfried Bounsi <wilcoln99@gmail.com>
2022-08-27 05:49:56 +0100

    refactor bart with knowledge

--- src/utils/transformers.py
+++ src/utils/transformers.py
@@ -4,11 +4,12 @@ from typing import Optional, List, Tuple, Union, Dict, Any
 import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss
-from transformers import BartConfig, BartModel as BaseBartModel
+from transformers import BartConfig, BartModel
 from transformers.modeling_outputs import Seq2SeqLMOutput, Seq2SeqModelOutput, BaseModelOutput
-from transformers.models.bart.modeling_bart import shift_tokens_right, BartPretrainedModel
+from transformers.models.bart.modeling_bart import shift_tokens_right, BartPretrainedModel, BartModel
 from transformers.utils import logging, ModelOutput
 
+from src.settings import settings
 from src.utils.attention_nle import AttentionNLE
 
 logger = logging.get_logger(__name__)
@@ -27,105 +28,16 @@ class BartModelOutput(Seq2SeqModelOutput):
     transformed_encoder_last_hidden_state: Optional[torch.Tensor] = None
 
 
-class BartModel(BaseBartModel):
-    def __init__(self, config: BartConfig):
-        super().__init__(config)
-        self.fusion_head = nn.Linear(2 * self.config.d_model, 1)
-        self.transform = nn.Identity()
-
-    def forward(
-            self,
-            input_ids: torch.LongTensor = None,
-            attention_mask: Optional[torch.Tensor] = None,
-            decoder_input_ids: Optional[torch.LongTensor] = None,
-            decoder_attention_mask: Optional[torch.LongTensor] = None,
-            head_mask: Optional[torch.Tensor] = None,
-            decoder_head_mask: Optional[torch.Tensor] = None,
-            cross_attn_head_mask: Optional[torch.Tensor] = None,
-            encoder_outputs: Optional[List[torch.FloatTensor]] = None,
-            past_key_values: Optional[List[torch.FloatTensor]] = None,
-            inputs_embeds: Optional[torch.FloatTensor] = None,
-            decoder_inputs_embeds: Optional[torch.FloatTensor] = None,
-            use_cache: Optional[bool] = None,
-            output_attentions: Optional[bool] = None,
-            output_hidden_states: Optional[bool] = None,
-            return_dict: Optional[bool] = None,
-    ) -> Union[Tuple, BartModelOutput]:
-
-        # different to other models, Bart automatically creates decoder_input_ids from
-        # input_ids if no decoder_input_ids are provided
-        if decoder_input_ids is None and decoder_inputs_embeds is None:
-            if input_ids is None:
-                raise ValueError(
-                    "If no `decoder_input_ids` or `decoder_inputs_embeds` are "
-                    "passed, `input_ids` cannot be `None`. Please pass either "
-                    "`input_ids` or `decoder_input_ids` or `decoder_inputs_embeds`."
-                )
-
-            decoder_input_ids = shift_tokens_right(
-                input_ids, self.config.pad_token_id, self.config.decoder_start_token_id
-            )
-
-        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-        output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-        )
-        use_cache = use_cache if use_cache is not None else self.config.use_cache
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        if encoder_outputs is None:
-            encoder_outputs = self.encoder(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                head_mask=head_mask,
-                inputs_embeds=inputs_embeds,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
-        # If the user passed a tuple for encoder_outputs, we wrap it in a BaseModelOutput when return_dict=True
-        elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
-            encoder_outputs = BaseModelOutput(
-                last_hidden_state=encoder_outputs[0],
-                hidden_states=encoder_outputs[1] if len(encoder_outputs) > 1 else None,
-                attentions=encoder_outputs[2] if len(encoder_outputs) > 2 else None,
-            )
-
-        transformed_encoder_last_hidden_state = self.transform(encoder_outputs.last_hidden_state)
-
-        # Without knowledge
-        decoder_outputs = self.decoder(
-            input_ids=decoder_input_ids,
-            attention_mask=decoder_attention_mask,
-            encoder_hidden_states=transformed_encoder_last_hidden_state,
-            encoder_attention_mask=attention_mask,
-            head_mask=decoder_head_mask,
-            cross_attn_head_mask=cross_attn_head_mask,
-            past_key_values=past_key_values,
-            inputs_embeds=decoder_inputs_embeds,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        return BartModelOutput(
-            last_hidden_state=decoder_outputs.last_hidden_state,
-            transformed_encoder_last_hidden_state=transformed_encoder_last_hidden_state,
-            past_key_values=decoder_outputs.past_key_values,
-            decoder_hidden_states=decoder_outputs.hidden_states,
-            decoder_attentions=decoder_outputs.attentions,
-            cross_attentions=decoder_outputs.cross_attentions,
-            encoder_last_hidden_state=encoder_outputs.last_hidden_state,
-            encoder_hidden_states=encoder_outputs.hidden_states,
-            encoder_attentions=encoder_outputs.attentions,
-        )
-
-
 class BartWithKnowledgeModel(BartModel):
     def __init__(self, config: BartConfig):
         super().__init__(config)
         self.attention = AttentionNLE()
+        self.fusion_head = nn.Linear(2 * config.d_model, 1)
+        self.transform = nn.Sequential(
+            nn.Linear(settings.sent_dim, settings.sent_dim),
+            nn.ReLU(),
+            nn.Linear(settings.sent_dim, settings.sent_dim),
+        )
 
     def forward(
             self,
@@ -445,9 +357,7 @@ class BartWithKnowledgeForConditionalGeneration(BartForConditionalGeneration):
             return_dict=return_dict,
             knowledge_embedding=knowledge_embedding,
         )
-
-        # With knowledge
-        lm_logits = self.lm_head(outputs.last_hidden_state) + self.final_logits_bias
+        lm_logits = self.lm_head(outputs[0]) + self.final_logits_bias
 
         masked_lm_loss = None
         if labels is not None:
