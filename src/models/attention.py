23f0c959b95ad3a548f8bcdc84ffdc2d343f501e
Wilfried Bounsi <wilcoln99@gmail.com>
2022-08-29 18:29:49 +0100

    rename align dim

--- src/models/attention.py
+++ src/models/attention.py
@@ -18,13 +18,13 @@ class Attention(nn.Module):
         super().__init__()
 
         assert settings.hidden_dim % settings.num_attn_heads == 0, 'Hidden dim must be divisible by num attention heads'
-        self.hidden_dim = settings.hidden_dim // settings.num_attn_heads
+        self.align_dim = settings.hidden_dim // settings.num_attn_heads
         self.k_proj_list = nn.ModuleList([
-            nn.Linear(settings.sent_dim, self.hidden_dim)
+            nn.Linear(settings.sent_dim, self.align_dim)
             for _ in range(settings.num_attn_heads)
         ])
         self.s_proj_list = nn.ModuleList([
-            nn.Linear(settings.sent_dim, self.hidden_dim)
+            nn.Linear(settings.sent_dim, self.align_dim)
             for _ in range(settings.num_attn_heads)
         ])
 
@@ -47,7 +47,7 @@ class Attention(nn.Module):
             S = self.s_proj_list[i](sentences)  # (batch_size, hidden_dim)
             K = self.k_proj_list[i](knowledge)  # (knowledge_size, hidden_dim)
             # scaled dot product attention
-            alignment_weights = S @ K.T / math.sqrt(self.hidden_dim)  # (batch_size, knowledge_size)
+            alignment_weights = S @ K.T / math.sqrt(self.align_dim)  # (batch_size, knowledge_size)
             attention_weights = torch.softmax(alignment_weights, dim=1)  # (batch_size, knowledge_size)
             attention_output = attention_weights @ knowledge  # (batch_size, hidden_dim)
             # save attention head weights and outputs
