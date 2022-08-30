480da6e767bdd48a65bc135b3ff0d1dd328dfe1a
Wilfried Bounsi <wilcoln99@gmail.com>
2022-08-30 18:00:47 +0100

    add kam plot

--- src/experiments/knowledge_attention_map.py
+++ src/experiments/knowledge_attention_map.py
@@ -2,12 +2,15 @@ import os.path as osp
 
 import pandas as pd
 import torch
+from icecream import ic
+from matplotlib import pyplot as plt
 
-from src.conceptnet import conceptnet
+# from src.conceptnet import conceptnet
 from src.datasets.nl import get_loader
 from src.models.sane import SANE
+import seaborn as sns
 from src.settings import settings
-from src.utils.embeddings import frozen_bart_tokenizer
+# from src.utils.embeddings import frozen_bart_tokenizer
 
 
 def compute_knowledge_attention_map(inputs, model):
@@ -35,14 +38,35 @@ def compute_knowledge_attention_map(inputs, model):
         df.to_csv(csv_path)
 
 
+def plot_knowledge_attention_map(results_path, fname):
+    # Load attention maps
+    df = pd.read_csv(osp.join(results_path, fname), index_col=[0])
+
+    column_mapping = {t: f'T{i}' for i, t in enumerate(df.columns)}
+    index_mapping = {s: f'S{i}' for i, s in enumerate(df.index)}
+    df.rename(columns=column_mapping, inplace=True)
+    df.index = list(index_mapping.values())
+    # Plot attention maps
+    fig, ax = plt.subplots(figsize=(20, 4))
+    sns.set(rc={'text.usetex': True})
+    sns.heatmap(df, ax=ax, cmap='viridis')
+    plt.tight_layout()
+    plt.savefig(osp.join(results_path, 'knowledge_attention_map.pdf'))
+    plt.savefig(osp.join(results_path, 'knowledge_attention_map.png'))
+    plt.show()
+
+
 if __name__ == '__main__':
     # Get test dataloader
-    inputs = next(iter(get_loader('val')))
+    # inputs = next(iter(get_loader('val')))
 
     # Load model
-    model = SANE().to(settings.device)
-    results_path = settings.input_dir
-    model.load_state_dict(torch.load(osp.join(results_path, 'model.pt')))
-    model.eval()
+    # model = SANE().to(settings.device)
+    # results_path = settings.input_dir
+    # model.load_state_dict(torch.load(osp.join(results_path, 'model.pt')))
+    # model.eval()
+    # compute_knowledge_attention_map(inputs, model)
+    fname = 'knowledge_attention_map_5x5_1.csv'
 
-    compute_knowledge_attention_map(inputs, model)
+    results_path = settings.results_dir
+    plot_knowledge_attention_map(results_path, fname)
