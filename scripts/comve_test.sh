923ee020640f39dbeb8fe92ecd75f3ec537e0cd8
Wilfried Bounsi <wilcoln99@gmail.com>
2022-09-01 11:54:51 +0100

    update scripts

--- scripts/comve_test.sh
+++ scripts/comve_test.sh
@@ -18,13 +18,13 @@
 #SBATCH --mail-user=wilfried.bounsi@cs.ox.ac.uk
 
 # SANE - Done
-input_dir='results/trainers/2022-08-30_16-08-04_306433_model=SANE_dataset=comve_num_epochs=15_batch_size=128_lr=5e-05_weight_decay=0.01_sent_dim=768_'
-python src/experiments/suite.py --dataset=comve --input_dir=$input_dir
+# input_dir='results/trainers/2022-08-30_16-08-04_306433_model=SANE_dataset=comve_num_epochs=15_batch_size=128_lr=5e-05_weight_decay=0.01_sent_dim=768_'
+# python src/experiments/suite.py --dataset=comve --input_dir=$input_dir
 
 # NO-GNN - Done
 input_dir='results/trainers/2022-08-30_15-15-17_061483_model=SANE_dataset=comve_num_epochs=15_batch_size=128_lr=5e-05_weight_decay=0.01_sent_dim=768_'
 python src/experiments/suite.py --dataset=comve --input_dir=$input_dir --no_gnn
 
 # NO-KNOWLEDGE - Running
-input_dir='results/trainers/2022-08-31_16-46-33_830328_model=SANENoKnowledge_dataset=comve_num_epochs=5_batch_size=128_lr=5e-05_weight_decay=0.01_sen'
-python src/experiments/suite.py --dataset=comve --input_dir=$input_dir --no_knowledge
+# input_dir='results/trainers/2022-08-31_16-46-33_830328_model=SANENoKnowledge_dataset=comve_num_epochs=5_batch_size=128_lr=5e-05_weight_decay=0.01_sen'
+# python src/experiments/suite.py --dataset=comve --input_dir=$input_dir --no_knowledge
